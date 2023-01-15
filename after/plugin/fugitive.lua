182a4aa179c728dd0c99a5f808787716c09b4247
Nicholas Nguyen <nguyennicholas8@gmail.com>
2023-01-15 13:54:57 -0800

    Added fugitive keymappings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,3 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', function() vim.cmd.Git('status') end)
+vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git('push origin main') end)
