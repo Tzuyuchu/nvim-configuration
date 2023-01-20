b281831b092795627ac65fa8cd8cd86ff0ff8daa
Nicholas Nguyen <nguyennicholas8@gmail.com>
2023-01-19 23:08:35 -0800

    Airline modifications

--- after/plugin/airline.lua
+++ after/plugin/airline.lua
@@ -1,8 +1,19 @@
 vim.cmd([[
 let g:airline_powerline_fonts = 1
-let g:airline_left_sep=''
+let g:airline_left_sep = ''
+let g:airline_left_alt_sep = ''
+let g:airline_right_sep = ''
+let g:airline_right_alt_sep = ''
+let g:airline_symbols.branch = ''
+let g:airline_symbols.colnr = ' ℅:'
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = ' '
+let g:airline_symbols.maxlinenr = ' ☰ '
+let g:airline_symbols.dirty='⚡'
 let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#tabline#formatter = 'unique_tail'
 let g:airline#extensions#tabline#left_sep = ''
 let g:airline#extensions#tabline#left_alt_sep = ''
+
+let g:airline#extensions#branch#enabled = 1
 ]])
